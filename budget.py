0cf16e99582f5058711f04aa761fc9ce58837726
Alessandro Allegranzi <allegranzia@gmail.com>
2022-12-11 15:18:14 -0500

    validating some user input

--- budget.py
+++ budget.py
@@ -13,11 +13,13 @@ class Budget(object):
     info(additional=""):
         Prints the person's name and age.
     """
-    def __init__(self, income: float):
+    def __init__(self, income: float, budget_map: dict = {"necessities": 50, "savings": 20, "discretionary": 30}):
         """
         The constructor for Budget class.
   
         Parameters:
-           income (float): A person's total monthly income  
+           income (float): A person's total monthly income
+
         """
         self.income = income
+        self.budget_map = budget_map
--- main.py
+++ main.py
@@ -2,12 +2,42 @@
 Name: Python Budget Calculator
 Author: Alessandro Allegranzi
 """
+import numbers
 from budget import Budget
 
 def main():
-    income  = input('Please enter your monthly income: ')
-    budget = Budget(float(income))
-    print("Your income is: ", budget.income)
+    """Main function running the logic of the terminal application"""
+
+    income = input('Please enter your monthly income: ')
+    
+    while not income_is_valid(income):
+        income = input('There was a problem validating your income, please try again: ')
+
+    standard_budget = input("Would you like to use a standard 50/30/20 budget?[y/n]: ")
+
+    while not yes_or_no_valid(standard_budget):
+        standard_budget = input("[y/n]?: ")
+
+    if standard_budget == "y":
+        budget = Budget(float(income))
+        print("Your income is: ", budget.income)
+        print("Your budget target is: ", budget.budget_map)
+
+
+def income_is_valid(income: str) -> bool:
+    """Validating user provided income"""
+    try:
+        income = float(income)
+        return True
+    except:
+        return False
+
+def yes_or_no_valid(selection: str) -> bool:
+    """Validating user provided income"""
+    if selection == "y" or selection == "n":
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
     main()
