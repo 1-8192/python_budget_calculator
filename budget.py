7dde0929f9bb405956103779923056395911810b
Alessandro Allegranzi <allegranzia@gmail.com>
2022-12-11 14:21:27 -0500

    created base files and tested importing

---
+++ budget.py
@@ -0,0 +1,3 @@
+class Budget(object):
+    def __init__(self):
+        self.test = 'test'
---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+Name: Python Budget Calculator
+Author: Alessandro Allegranzi
+"""
+from budget import Budget
+
+def main():
+    test = Budget()
+    print(test.test)
+
+if __name__ == '__main__':
+    main()
